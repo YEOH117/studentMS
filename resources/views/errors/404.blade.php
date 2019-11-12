ffc677c11110f61dc71bbb6f62273ff90f2155cc
YEOH117 <18965720201@189.cn>
2019-11-12 13:58:27 +0800

    使用腾讯公益404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,13 +2,10 @@
 <head>
     <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
     <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js" charset="utf-8" homePageUrl="http://120.78.168.8" homePageName="返回首页"></script>
 </head>
-<body style="background-image: url('/img/dog-690176_1920.jpg')">
-<div class="col-md-12" style="height:100%">
-    <div style="height:65%">
-    </div>
-    <span style="font-size: 90px;color: #ffffff;padding-left: 1200px;">404,未找到页面</span>
-</div>
+<body>
+
 
 
 
